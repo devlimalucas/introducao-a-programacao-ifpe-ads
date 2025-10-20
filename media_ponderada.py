588c8490608f853887cf430034288854af57ffaf
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-20 19:26:28 -0300

    feat: media ponderada

---
+++ media_ponderada.py
@@ -0,0 +1,13 @@
+import sys
+
+primeira_nota = float(input("Digite a primeira nota: "))
+peso_primeira_nota = float(input("Digite o peso da segunda nota: "))
+segunda_nota = float(input("Digite a segunda nota: "))
+peso_segunda_peso = float(input("Digite o peso da segunda nota: "))
+
+def media_ponderada():
+  calc_primeira_nota = primeira_nota * peso_primeira_nota
+  calc_segunda_nota = segunda_nota * peso_primeira_nota
+  print((calc_primeira_nota + calc_segunda_nota) / (peso_primeira_nota + peso_segunda_peso))
+
+media_ponderada()
